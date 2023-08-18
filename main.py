c7ef3ba914adfe6e07f294f8e03d37b4b5f955d0
RFHamster <rfcf10@gmail.com>
2023-08-18 19:22:53 -0300

    terminando o sistema de queue

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import discord
+from discord.ext import commands
 
-intents = discord.Intents.default()  # Define os intents padrão
-intents.message_content = True       # Permite o acesso ao conteúdo das mensagens
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix='t!', intents=intents)
 
-client = discord.Client(intents=intents)
+actualQueue = []  # Sua lista para armazenar os jogadores na fila
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'Bot pronto como {client.user.name}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command()
+async def queue(msg):
+    actualQueue.append(msg.author)
+    await msg.send(f'{msg.author.mention} foi adicionado à fila.')
+    if actualQueue.len >= 10:
+        start(actualQueue[:10])
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@client.command()
+async def show_queue(msg):
+    queue_list = "\n".join([member.name for member in actualQueue])
+    await msg.send(f"Jogadores na fila:\n{queue_list}")
+
+def start(players):
+    time1 = []
+    time2 = []
 
 client.run("MTE0MjIwMDkwMTU2NjcyNjE0NA.GIDm8X.GkOmoPQO15-c0PJMnWwClqtgUZ5GzHiS_1rSOI")
